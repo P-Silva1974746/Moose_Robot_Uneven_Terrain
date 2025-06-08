ec529082ea178e93b5549989dbc78244d046d3c0
P-Silva1974746 <pedrofsilva5164@gmail.com>
2025-06-08 01:16:16 +0100

    FIX: these script are now functions that can be imported which in case o create_env is important to randomize maps during training

--- env_maker.py
+++ env_maker.py
@@ -5,61 +5,92 @@ import matplotlib.pyplot as plt
 import re
 from perlin_noise import PerlinNoise
 
-def replacer(match):
-    return f"{match.group(1)}{formated_heights}{match.group(3)}"
 
-PATH_FILE="./worlds/moose_demo.wbt"
-with open(file=PATH_FILE,mode='r') as f:
-    data= f.read()
+def create_perlin_map(map_w=200, map_h=200, path_file="./worlds/moose_demo.wbt", path_new_file="worlds/moose_perlin.wbt"):
+    with open(file=path_file, mode='r') as f:
+        data= f.read()
 
-MAP_W=200
-MAP_H=200
+    # find the height elevation map
+    match = re.search(r'height\s*\[\s*(.*?)\s]',data, re.DOTALL)
 
+    if match:
+        # extract string of numbers
+        heights_strings=match.group(1)
 
-# find the height elevation map
-match = re.search(r'height\s*\[\s*(.*?)\s]',data, re.DOTALL)
+        #convert the values
+        heights=np.array([float(s.strip()) for s in heights_strings.split(',') if s.strip()])
 
-if match:
-    # extract string of numbers
-    heights_strings=match.group(1)
+        #---------------------CODE TO ALTER THE ELEVATION MAP------------------------#
+        noise_scale=0.020
+        perlin_noise = PerlinNoise(dimension=2,octaves=1,unbias=False)
+        #modifed_heights=heights/3
+        modified_heights=[]
 
-    #convert the values
-    heights=np.array([float(s.strip()) for s in heights_strings.split(',') if s.strip()])
+        max_dist=np.hypot(map_w/2,map_h/2)
 
-    #---------------------CODE TO ALTER THE ELEVATION MAP------------------------#
-    noise_scale=0.020
-    perlin_noise = PerlinNoise(dimension=2,octaves=1,unbias=False)
-    #modifed_heights=heights/3
-    modified_heights=[]
-
-    max_dist=np.hypot(MAP_W/2,MAP_H/2)
-
-    # 200 is the size of our elevation grid this could be done with code if we read it from the .wbt file directly
-    for j in range (MAP_H):
-        for i in range (MAP_H):
-            dist_x= MAP_W/2 -i
-            dist_y= MAP_H/2 -j
-
-            #the 0.8 in the dist_y makes it less important meaning the elevation in the map is not a perfect circle
-            dist= np.hypot(dist_x,0.8*dist_y)
-            new_height=30-45*dist/max_dist
-            new_height+= perlin_noise((i+random.random())*noise_scale,(j+random.random())*noise_scale)*5
-            #new_height=0
-
-            modified_heights.append(new_height)
-    #---------------------CODE TO ALTER THE ELEVATION MAP------------------------#
-
-    formated_heights=',\n '.join(f"{height:.6f}" for height in modified_heights)
-
-    #replace the data
-    updated_data=re.sub(r'(height\s*\[\s*)(.*?)(\s*\])',
-                        replacer,
-                        data,
-                        flags=re.DOTALL
-    )
-
-    PATH_NEW_FILE="worlds/moose_perlin_circle.wbt"
-    with open(PATH_NEW_FILE,mode='w') as f:
-        f.write(updated_data)
+        # get random point to create the spawn zone
+        x = random.randint(0,map_w-1)
+        y = random.randint(0,map_h-1)
+        size_spawn=10
+        spawn_heights=[]
 
 
+        # 200 is the size of our elevation grid this could be done with code if we read it from the .wbt file directly
+        for j in range (map_h):
+            for i in range (map_w):
+                dist_x= map_w/2 -i
+                dist_y= map_h/2 -j
+
+                #the 0.8 in the dist_y makes it less important meaning the elevation in the map is not a perfect circle
+                dist= np.hypot(dist_x,0.8*dist_y)
+
+                # To make a circular map uncomment this line and add the noise instead of assigning it
+                #new_height=30-45*dist/max_dist
+                new_height= perlin_noise((i+random.random())*noise_scale,(j+random.random())*noise_scale)*5
+                if (x-size_spawn)<=i<(x+size_spawn) and (y-size_spawn)<=j<(y+size_spawn):
+                    #print(f"The point {(i,j)} is part of the spawn")
+                    spawn_heights.append(new_height)
+
+                modified_heights.append(new_height)
+
+        spawn_heights = np.array(spawn_heights)
+        spawn_height= np.median(spawn_heights)
+
+        for spawn_i  in range(max(0, x-size_spawn), min(x+size_spawn,map_w-1)):
+            for spawn_j in range(max(0, y-size_spawn) , min(y+size_spawn, map_h-1)):
+                modified_heights[spawn_i + spawn_j*map_w]=spawn_height
+        #---------------------CODE TO ALTER THE ELEVATION MAP------------------------#
+
+        formated_heights=',\n '.join(f"{height:.6f}" for height in modified_heights)
+
+        def replacer(match):
+            return f"{match.group(1)}{formated_heights}{match.group(3)}"
+
+        #replace the data
+        updated_data=re.sub(r'(height\s*\[\s*)(.*?)(\s*\])',
+                            replacer,
+                            data,
+                            flags=re.DOTALL
+        )
+
+        # setting the robot translation to the spawn area
+        pos_x = float (x)
+        pos_y = float(y)
+        pos_z = float(spawn_height + 0.5)
+
+        # Replace MOOSEROBOT translation
+        updated_data = re.sub(
+            r'(DEF\s+MOOSEROBOT\s+Robot\s*{(?:[^{}]*\{[^{}]*\}[^{}]*)*?[^}]*?)translation\s+[-\d.eE+]+\s+[-\d.eE+]+\s+[-\d.eE+]',
+            lambda match: f'{match.group(1)}translation {pos_x:.6f} {pos_y:.6f} {pos_z:.6f}',
+            updated_data,
+            flags=re.DOTALL
+        )
+
+        with open(path_new_file,mode='w') as f:
+            f.write(updated_data)
+
+        return x,y, spawn_height
+
+
+if __name__ == "__main__":
+    create_perlin_map(path_new_file="worlds/moose_perlin_teste.wbt")
--- height_eval.py
+++ height_eval.py
@@ -2,66 +2,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
-PATH_FILE="worlds/moose_perlin.wbt"
-with open(file=PATH_FILE,mode='r') as f:
-    data= f.read()
 
 
-match = re.search(r'height\s*\[\s*(.*?)\s]',data, re.DOTALL)
+def plot_height_graphs(path_file="worlds/moose_perlin.wbt"):
+    with open(file=path_file,mode='r') as f:
+        data= f.read()
 
-if match:
-    # extract string of numbers
-    heights_strings=match.group(1)
 
-    heights=np.array([float(s.strip()) for s in heights_strings.split(',') if s.strip()])
+    match = re.search(r'height\s*\[\s*(.*?)\s]',data, re.DOTALL)
 
-    n_entries=len(heights)
-    print()
-    print(f"Number of entries in the heights: {n_entries}")
+    if match:
+        # extract string of numbers
+        heights_strings=match.group(1)
 
-    mean=np.mean(heights)
-    median=np.median(heights)
-    std=np.std(heights)
-    min_val=np.min(heights)
-    max_value=np.max(heights)
+        heights=np.array([float(s.strip()) for s in heights_strings.split(',') if s.strip()])
 
-    print()
-    print("Statistics:")
-    print(f"Mean: {mean}")
-    print(f"Median: {median}")
-    print(f"Standard Deviation: {std}")
-    print(f"Min height: {min_val}")
-    print(f"Max height: {max_value}")
+        n_entries=len(heights)
+        print()
+        print(f"Number of entries in the heights: {n_entries}")
 
-    plt.figure(figsize=(8,6))
-    plt.hist(heights,bins=30, edgecolor='black', alpha=0.75)
-    plt.title("Distribuition of Heights")
-    plt.xlabel("Height")
-    plt.ylabel("Frequency")
-    plt.grid(True)
-    plt.show()
+        mean=np.mean(heights)
+        median=np.median(heights)
+        std=np.std(heights)
+        min_val=np.min(heights)
+        max_value=np.max(heights)
 
-    # Elevation map
-    # dimenstion of our world which is a square
-    dim=int(np.sqrt(n_entries))
-    if dim*dim!=n_entries:
-        raise ValueError(f"The world given is not square, n_entries: {n_entries} is not equal to dim^2: {dim*dim}. Please set xDimension and yDimension manually.")
+        print()
+        print("Statistics:")
+        print(f"Mean: {mean}")
+        print(f"Median: {median}")
+        print(f"Standard Deviation: {std}")
+        print(f"Min height: {min_val}")
+        print(f"Max height: {max_value}")
 
-    height_map= np.zeros((dim,dim))
-    for j in range(dim):
-        for i in range(dim):
-            #index of the height arrays in .wbt files according to the elevation grid documentation of webots
-            index= i+j*dim
-            height_map[j,i]=heights[index]
+        plt.figure(figsize=(8,6))
+        plt.hist(heights,bins=30, edgecolor='black', alpha=0.75)
+        plt.title("Distribuition of Heights")
+        plt.xlabel("Height")
+        plt.ylabel("Frequency")
+        plt.grid(True)
+        plt.show()
 
-    plt.figure(figsize=(8,6))
-    cmap=plt.get_cmap('terrain')
-    img = plt.imshow(height_map, cmap=cmap, origin='lower')
-    plt.colorbar(img, label='Height')
-    plt.title("Height Map")
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.tight_layout()
-    plt.show()
-else:
-    print(f"No data found in the file: {PATH_FILE}")
+        # Elevation map
+        # dimenstion of our world which is a square
+        dim=int(np.sqrt(n_entries))
+        if dim*dim!=n_entries:
+            raise ValueError(f"The world given is not square, n_entries: {n_entries} is not equal to dim^2: {dim*dim}. Please set xDimension and yDimension manually.")
+
+        height_map= np.zeros((dim,dim))
+        for j in range(dim):
+            for i in range(dim):
+                #index of the height arrays in .wbt files according to the elevation grid documentation of webots
+                index= i+j*dim
+                height_map[j,i]=heights[index]
+
+        plt.figure(figsize=(8,6))
+        cmap=plt.get_cmap('terrain')
+        img = plt.imshow(height_map, cmap=cmap, origin='lower')
+        plt.colorbar(img, label='Height')
+        plt.title("Height Map")
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.tight_layout()
+        plt.show()
+    else:
+        print(f"No data found in the file: {path_file}")
+
+if __name__ == "__main__":
+    plot_height_graphs(path_file="worlds/moose_perlin_teste.wbt")
